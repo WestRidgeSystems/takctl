f7e7bd4223ef70348ab9f491d1d2a67d8eb2f9e9
wlfgang <14792753+wlfgang@users.noreply.github.com>
2024-12-12 08:58:40 -0500

    Set cert alias in truststore and check if existing

--- src/main/java/com/westridgesystems/commands/creds/TruststoreCommand.java
+++ src/main/java/com/westridgesystems/commands/creds/TruststoreCommand.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.file.Path;
 import java.security.KeyStore;
+import java.security.cert.X509Certificate;
 
 @CommandLine.Command(name = "truststore", mixinStandardHelpOptions = true,
         description = "Manually add a server's CA cert to the truststore.")
@@ -22,16 +23,16 @@ public class TruststoreCommand implements Runnable {
     @Override
     public void run() {
         Path configPath = FileUtil.getConfigDir();
-
-        // Create the truststore if it doesn't exist
         String truststoreFilename = "truststore.jks";
         Path truststorePath = configPath.resolve(truststoreFilename);
         String truststorePassword = "changeit";
+
+        // Create the truststore if it doesn't exist
         if (!truststorePath.toFile().exists()) {
             try (FileOutputStream fos = new FileOutputStream(truststorePath.toFile())) {
                 KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
                 keyStore.load(null, null);  // Initialize an empty keystore
-                keyStore.store(fos, truststorePassword.toCharArray());  // Use a default password "changeit"
+                keyStore.store(fos, truststorePassword.toCharArray());
                 spec.commandLine().getOut().println("Truststore created at: " + truststorePath);
             } catch (Exception e) {
                 spec.commandLine().getErr().println("Error creating truststore: " + e.getMessage());
@@ -43,19 +44,29 @@ public class TruststoreCommand implements Runnable {
             KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
             trustStore.load(fis, truststorePassword.toCharArray());
 
-            // Import CA certificate into the truststore
+            // Open the cert
             Path caCertPath = Path.of(caCertFile);
             try (FileInputStream caInputStream = new FileInputStream(caCertPath.toFile())) {
                 java.security.cert.Certificate caCertificate = java.security.cert.CertificateFactory.getInstance("X" +
                                 ".509")
                         .generateCertificate(caInputStream);
-                trustStore.setCertificateEntry("server-alias", caCertificate);
 
-                // Save the updated keystore back to the file
-                try (FileOutputStream out = new FileOutputStream(truststorePath.toFile())) {
-                    trustStore.store(out, truststorePassword.toCharArray());
-                    spec.commandLine().getOut().println("CA certificate imported into truststore with alias: " +
-                            "server-alias");
+                if (caCertificate instanceof X509Certificate x509Certificate) {
+                    String alias = x509Certificate.getSubjectX500Principal().getName();
+
+                    // Check if the truststore already has an entry under the alias
+                    if (trustStore.containsAlias(alias)) {
+                        spec.commandLine().getOut().println("Truststore has an existing entry for " + alias +
+                                ". Use keytool to remove it manually before importing.");
+                        return;
+                    }
+                    trustStore.setCertificateEntry(alias, x509Certificate);
+
+                    // Save the file
+                    try (FileOutputStream out = new FileOutputStream(truststorePath.toFile())) {
+                        trustStore.store(out, truststorePassword.toCharArray());
+                        spec.commandLine().getOut().println("CA certificate imported into truststore: " + alias);
+                    }
                 }
             }
         } catch (Exception e) {
